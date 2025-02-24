42810c7b6eab780acaec5c08a48b507c3dd8fd9a
ray__s <Rayy_Bush@proton.me>
2025-02-24 09:17:22 +0300

    Задание 50

--- src/App.vue
+++ src/App.vue
@@ -8,43 +8,21 @@ import TheWelcome from './components/TheWelcome.vue'
 		<table border="1">
 			<thead>
 				<tr>
-					<th>ID</th>
 					<th>Имя</th>
-					<th>Зарплата</th>
+					<th>Фамилия</th>
 					<th>Возраст</th>
-					<th>Удалить</th>
-					<th>Редактировать</th>
 				</tr>
 			</thead>
 			<tbody>
 				<Employee
 					v-for="(user, index) in users"
 					:key="user.id"
-					:user="user"
-					:index="index"
-					@remove="removeUser"
-					@edit="editUser"
+					:firstName="user.firstName"
+					:lastName="user.lastName"
+					:age="user.age"
 				/>
 			</tbody>
 		</table>
-
-		<!-- Модальное окно для редактирования -->
-		<div v-if="isEditing" class="modal">
-			<div class="modal-content">
-				<h2>Редактировать пользователя</h2>
-				<label for="name">Имя:</label>
-				<input type="text" v-model="editedUser.name" />
-
-				<label for="salary">Зарплата:</label>
-				<input type="number" v-model="editedUser.salary" />
-
-				<label for="age">Возраст:</label>
-				<input type="number" v-model="editedUser.age" />
-
-				<button @click="saveUser">Сохранить</button>
-				<button @click="cancelEdit">Отменить</button>
-			</div>
-		</div>
 	</div>
 </template>
 
@@ -58,55 +36,14 @@ export default {
 	data() {
 		return {
 			users: [
-				{ id: 1, name: 'name1', salary: 100, age: 30 },
-				{ id: 2, name: 'name2', salary: 200, age: 40 },
-				{ id: 3, name: 'name3', salary: 300, age: 50 },
+				{ id: 1, firstName: 'John', lastName: 'Doe', age: 30 },
+				{ id: 2, firstName: 'Jane', lastName: 'Smith', age: 40 },
+				{ id: 3, firstName: 'Michael', lastName: 'Johnson', age: 50 },
 			],
-			isEditing: false,
-			editedUser: {},
-			editingIndex: null,
 		}
-	},
-	methods: {
-		removeUser(index) {
-			this.users.splice(index, 1)
-		},
-		editUser(index) {
-			this.editedUser = { ...this.users[index] }
-			this.editingIndex = index
-			this.isEditing = true
-		},
-		saveUser() {
-			this.users[this.editingIndex] = { ...this.editedUser }
-			this.isEditing = false
-		},
-		cancelEdit() {
-			this.isEditing = false
-		},
 	},
 }
 </script>
-
-<style scoped>
-.modal {
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background: rgba(0, 0, 0, 0.5);
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-
-.modal-content {
-	background: white;
-	padding: 20px;
-	border-radius: 5px;
-	width: 300px;
-}
-</style>
 
 <style scoped>
 #app {
