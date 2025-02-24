05fe78cf572ad6f1ddfe04d03a5b16d806226863
ray__s <Rayy_Bush@proton.me>
2025-02-24 08:59:57 +0300

    Задание 25

--- src/App.vue
+++ src/App.vue
@@ -4,38 +4,15 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-	<!-- №1: Список ссылок -->
-	<ul>
-		<li v-for="item in hrefs" :key="item.href">
-			<a :href="item.href">{{ item.text }}</a>
-		</li>
-	</ul>
-
-	<!-- №2: Таблица с продуктами -->
-	<table>
-		<tr v-for="product in products" :key="product.name">
-			<td>{{ product.name }}</td>
-			<td>{{ product.price }}</td>
-			<td>{{ product.quantity }}</td>
-		</tr>
-	</table>
+	<div>
+		<p v-for="n in 30" :key="n">{{ n }}</p>
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
-		return {
-			hrefs: [
-				{ href: '1.html', text: 'text1' },
-				{ href: '2.html', text: 'text2' },
-				{ href: '3.html', text: 'text3' },
-			],
-			products: [
-				{ name: 'product1', price: 100, quantity: 5 },
-				{ name: 'product2', price: 200, quantity: 4 },
-				{ name: 'product3', price: 300, quantity: 3 },
-			],
-		}
+		return {}
 	},
 }
 </script>
